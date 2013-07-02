00249ce671a682e5af938c42a32354f08f411cfb
Marcos Web <Marcos Web@Marcos-Notebook>
2013-07-02 01:41:50 -0300

    Busca direto no Site da Caixa e faz pesquisa por concurso

--- lotofacil/index.php
+++ lotofacil/index.php
@@ -1,11 +1,15 @@
 <?php
-require_once 'lotofacil.class.php';
-$lf = new Lotofacil();
-$lf->conferir_jogos();
-$res = $lf->extrai_resultados(911); //número do sorteio (concurso).
-if ($res) var_dump($res);
-if ($lf->tem_erros()){
-    var_dump($lf->pega_erros());
+
+require_once 'Loteria.php';
+
+$l = new Loteria();
+$l->loteria('lotofacil');
+$l->executa(918); // número do concurso
+
+if (!$l->temErros()) {
+    var_dump($l->numerosSorteados());
+} else {
+    $erros = $l->pegaErros();
+    var_dump($erros);
 }
-unset($lf);
-?>
+
